fdbffa9e722461aad8871bb65927b1a4c9c30142
Rossana1603 <rossana1603@gmail.com>
2014-10-27 16:10:19 -0500

    Forum

---
+++ Forum/Forum.WebApi/Controllers/ForumController.cs
@@ -0,0 +1,25 @@
+﻿using Forum.WebApi.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Forum.WebApi.Controllers
+{
+    public class ForumController : ApiController
+    {
+        private static IList<ForumModel> lst = new List<ForumModel>()
+        {
+            new ForumModel
+            {
+                ForumID = 12345,
+                Titulo = "Dragones de Komodo",
+                Contenido = "Los dragones de Komodo son criaturas espectaculares",
+                FechaCreacion = System.DateTime.Now
+            },
+        };
+
+        public IEnumerable<ForumModel> Get()
+        {
+            return lst;
+        }
+	}
+}
---
+++ Forum/Forum/Controllers/ForumController.cs
@@ -0,0 +1,54 @@
+﻿using Forum.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forum.Controllers
+{
+    public class ForumController : Controller
+    {
+        private HttpClient client;
+
+        private HttpClient GetHttpClient()
+        {
+            client = new HttpClient();
+            
+            // New code:
+            client.BaseAddress = new Uri("http://localhost:51713/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            return client;
+        }
+
+
+        private async Task<IEnumerable<ForumModel>> Test()
+        {
+            IEnumerable<ForumModel> lstForums = new List<ForumModel>();
+            using (var client = GetHttpClient())
+            {
+                // New code:
+                HttpResponseMessage response = await client.GetAsync("api/forum");
+                if (response.IsSuccessStatusCode)
+                {
+                    lstForums = await response.Content.ReadAsAsync<IEnumerable<ForumModel>>();
+                }
+            }
+
+            return lstForums;
+        }
+
+        //
+        // GET: /Forum/
+        public ActionResult Index()
+        {
+            var test = Test();
+            return View();
+        }
+	}
+}
