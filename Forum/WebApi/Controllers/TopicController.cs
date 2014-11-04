b55d6ed24b6e818ff97e2d13bff002a398f59a41
Rossana1603 <rossana1603@gmail.com>
2014-11-04 09:54:09 -0500

    Controllers/TopicController.cs

--- Forum/WebApi/Controllers/TopicController.cs
+++ Forum/WebApi/Controllers/TopicController.cs
@@ -1,64 +1,42 @@
 ﻿using Forum.Persistence.DataAccess;
-using Forum.WebApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Linq;
+using Forum.Persistence.Domain;
 
-namespace Forum.WebApi.Controllers
+namespace WebApi.Controllers
 {
     public class TopicController : ApiController
     {
-        private static IList<Topic> lst = new List<Topic>()
-        {
-            new Topic
-            {
-                Id = 12345,
-                Title = "Dragones de Komodo",
-                Content = "Los dragones de Komodo son criaturas espectaculares",
-                CreateDate = System.DateTime.Now,
-                Tags = new List<string>{ "#Dragones", "#Espectacular", "#Criaturas"}
-            },
-            new Topic
-            {
-                Id = 98765,
-                Title = "Las Artes Oscuras",
-                Content = "Las clases del profesor Snape son muy interesantes.",
-                CreateDate = System.DateTime.Now
-            },
-        };
+        private readonly TopicRepository _topicRepository = new TopicRepository(new ForumContext());
 
         public IEnumerable<Topic> Get()
         {
-            return lst;
+            return _topicRepository.Query();
         }
 
         public HttpResponseMessage Get(int id)
         {
-            var topic = lst.FirstOrDefault(x => x.Id == id);
+            var topic = _topicRepository.Get(id);
 
-            if(topic == null) return Request.CreateResponse(HttpStatusCode.NotFound);
-
-            return Request.CreateResponse<Topic>(HttpStatusCode.Found, topic);
+            return (topic == null) ? Request.CreateResponse(HttpStatusCode.NotFound) : Request.CreateResponse<Topic>(HttpStatusCode.Found, topic);
         }
 
-        public HttpResponseMessage GetTagsByTopicId(int id)
-        {
-            var tags = lst.FirstOrDefault(x => x.Id == id);
+        //public HttpResponseMessage GetTagsByTopicId(int id)
+        //{
+        //    var tags = lst.FirstOrDefault(x => x.Id == id);
 
-            if (tags == null) return Request.CreateResponse(HttpStatusCode.NotFound);
+        //    if (tags == null) return Request.CreateResponse(HttpStatusCode.NotFound);
 
-            return Request.CreateResponse<List<String>>(HttpStatusCode.Found, tags.Tags); 
-        }
+        //    return Request.CreateResponse<List<String>>(HttpStatusCode.Found, tags.Tags); 
+        //}
 
         public HttpResponseMessage Post(Topic topic)
         {
-            var newId = lst.Max(x => x.Id) + 1;
-            topic.Id = newId;
-            lst.Add(topic);
-
+            _topicRepository.Add(topic);
             var response = Request.CreateResponse<Topic>(HttpStatusCode.Created, topic);
             var uri = Url.Link("DefaultApi", new { id = topic.Id });
             response.Headers.Location = new Uri(uri);
@@ -67,15 +45,8 @@ namespace Forum.WebApi.Controllers
 
         public HttpResponseMessage Put(int id, Topic topic)
         {
-            var index = lst.ToList().FindIndex(x => x.Id == id);
-            if (index >= 0)
-            {
-                topic.Id = id;
-                lst[index] = topic;
-                return Request.CreateResponse(HttpStatusCode.NoContent);
-            }
-
-            return Request.CreateResponse(HttpStatusCode.NotFound);
+            var response = _topicRepository.Update(topic);
+            return (!response) ? Request.CreateResponse(HttpStatusCode.NotFound) : Request.CreateResponse(HttpStatusCode.NoContent);
         } 
 	}
 }
