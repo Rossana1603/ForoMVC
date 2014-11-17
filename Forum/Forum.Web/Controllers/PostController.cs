d0544bc816a33d0250f9e5c26afeca7b849183e7
Rossana1603 <rossana1603@gmail.com>
2014-11-17 15:52:00 -0500

    create post controller

---
+++ Forum/Forum.Web/Controllers/PostController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Forum.Persistence.Domain;
+using Forum.Web.Properties;
+using RestSharp;
+
+namespace Forum.Web.Controllers
+{
+    [Authorize]
+    public class PostController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GeTopicById(int id)
+        {
+            var client = new RestClient(Settings.Default.ForumApiUrl);
+            var request = new RestRequest("/api/post/GeTopicById/{id}", Method.GET);
+            request.AddParameter("id", id);
+            var response = client.Execute<Topic>(request);
+            var result = response.Data.Id;
+            return View();
+        }
+
+        public ActionResult GePostByTopicId(int id)
+        {
+            var client = new RestClient(Settings.Default.ForumApiUrl);
+            var request = new RestRequest("/api/post/GePostByTopicId/{id}", Method.GET);
+            request.AddParameter("id", id);
+            var response = client.Execute<List<Post>>(request);
+            var result = response.Data;
+            return View();
+        }
+	}
+}
--- Forum/Forum.Web/Models/TopicViewModel.cs
+++ Forum/Forum.Web/Models/TopicViewModel.cs
@@ -8,6 +8,7 @@ namespace Forum.Web.Models
 {
     public class TopicViewModel
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime CreateDate { get; set; }
