c0d662491ec72be1f577685a0dd56195925f5f8c
orlando1409 <orlando1409@gmail.com>
2014-11-17 16:00:15 -0500

    post view model

---
+++ Forum/Forum.Web/Models/PostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Forum.Persistence.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forum.Web.Models
+{
+    public class PostViewModel
+    {
+        public int TopicId { get; set; }
+        public virtual Topic Topic { get; set; }
+        public List<String> Tags { get; set; }
+        public string Content { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
