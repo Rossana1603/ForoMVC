8138dc27dfa7f61baa8f95483a8a57b9a717c02f
Rossana1603 <rossana1603@gmail.com>
2014-11-18 11:48:01 -0500

    Post methods

---
+++ Forum/Forum.Persistence/Migrations/201411181415027_Post List.cs
@@ -0,0 +1,16 @@
+namespace Forum.Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PostList : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Forum/Forum.Web/Controllers/PostController.cs
+++ Forum/Forum.Web/Controllers/PostController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
 using Forum.Persistence.Domain;
+using Forum.Web.Models;
 using Forum.Web.Properties;
 using RestSharp;
 
@@ -12,20 +14,53 @@ namespace Forum.Web.Controllers
     [Authorize]
     public class PostController : Controller
     {
-
-        public ActionResult Index()
+        public PostController()
         {
-            return View();
+            Mapper.CreateMap<Post,PostViewModel>()
+                .ForMember(vwm=>vwm.Tags,opt=>opt.Ignore());
         }
 
-        public ActionResult GePostByTopicId(int id)
+        public ActionResult GePostByTopicId(int topicId)
         {
             var client = new RestClient(Settings.Default.ForumApiUrl);
-            var request = new RestRequest("/api/post/GePostByTopicId/{id}", Method.GET);
-            request.AddParameter("id", id);
+            var request = new RestRequest("/api/post/GePostByTopicId/{topicId}", Method.GET);
+            request.AddParameter("topicId", topicId);
             var response = client.Execute<List<Post>>(request);
             var result = response.Data;
             return View();
+        }
+
+        public ActionResult AddPost()
+        {
+            var model = new PostViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddPost(PostViewModel post)
+        {
+            var client = new RestClient(Settings.Default.ForumApiUrl + "api/post/");
+            var request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
+
+            request.AddJsonBody(new Post()
+            {
+                TopicId = post.TopicId,
+                Content = post.Content,
+                Author = post.Author,
+                Tags =  post.Tags
+            });
+            var response = client.Execute<Post>(request);
+            return RedirectToAction("TopicList");
+        }
+
+        public ActionResult DeletePost(int id)
+        {
+            var client = new RestClient(Settings.Default.ForumApiUrl);
+            var request = new RestRequest("api/post/{id}", Method.DELETE);
+            request.AddParameter("id", id);
+            var queryResult = client.Execute<List<PostViewModel>>(request).Data;
+            return RedirectToAction("TopicList");
         }
 	}
 }
