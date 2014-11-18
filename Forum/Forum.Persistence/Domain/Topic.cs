037f9452005bf286a5d7057e45297cb7fe64db12
Rossana1603 <rossana1603@gmail.com>
2014-11-18 09:31:14 -0500

    new list post

--- Forum/Forum.Persistence/Domain/Topic.cs
+++ Forum/Forum.Persistence/Domain/Topic.cs
@@ -16,5 +16,6 @@ namespace Forum.Persistence.Domain
         public List<String> Tags { get; set; }
         public virtual Author Author { get; set; }
         public int AuthorId { get; set; }
+        public List<Post> Posts { get; set; }
     }
 }
