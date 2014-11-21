31cbb3aa3e6a5cdb4f4cd38e243cc83d1117c87b
orlando1409 <orlando1409@gmail.com>
2014-11-21 12:53:19 -0500

    new files

---
+++ Forum/Forum.Web/Resolver/HttpHelperResolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace Forum.Web.Resolver
+{
+    public static class HttpHelperResolver
+    {
+        public static void SaveImageAs(this HttpPostedFileBase file, string path, int width, int height)
+        {
+            WebImage img = new WebImage(file.InputStream);
+            if (img.Width > 100 || img.Height > 100)
+            {
+                img.Resize(100, 100);
+            }
+            img.Save(path);            
+        }
+    }
+}
