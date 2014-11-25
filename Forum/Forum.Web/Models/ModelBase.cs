7c37e43a2ffacd2030fb71ed64f9fd866142e483
Rossana1603 <rossana1603@gmail.com>
2014-11-25 09:57:52 -0500

    last changess

--- Forum/Forum.Web/Models/ModelBase.cs
+++ Forum/Forum.Web/Models/ModelBase.cs
@@ -8,5 +8,9 @@ namespace Forum.Web.Models
     public class CustomModelBase
     {
         public string AvatarFileName { get; set; }
+        public int PageCount { get; set; }
+        public int PageNumber { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
     }
 }
