659b4947911b9fc31b7f1475795ecdd1f59a324d
orlan1409 <orlando1409@gmail.com>
2014-11-26 22:45:53 -0500

    pending database updates

---
+++ Forum/Forum.Persistence/Migrations/201411262133593_initial.cs
@@ -0,0 +1,18 @@
+namespace Forum.Persistence.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Subscriptions", "Message", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Subscriptions", "Message");
+        }
+    }
+}
